e94fba6a398fbd0f2af7781a9994aee91fb39b85
Douglas <daniel.mejia@agilesolutions.com>
2022-01-16 00:02:00 -0600

    first styles

--- resources/views/noticia/form.blade.php
+++ resources/views/noticia/form.blade.php
@@ -1,31 +1,46 @@
-<h1>{{$modo}} Noticia</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="form-group container border p-3 mt-3 bg-white">
+        <h1 class="text-center">{{$modo}} Noticia</h1>
+        
+            @if(count($errors)>0)
+                <div class="alert alert-danger" role="alert">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li> {{$error}} </li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        
+            <label for="Titulo">Titulo de la Noticia</label>
+            <input class="form-control" type="text" name='Titulo' id='Titulo' 
+            value="{{ isset($noticia->Titulo)?$noticia->Titulo:old('Titulo') }}">
+            <br>
+        
+            <label for="Descripcion">Descripcion de la Noticia</label>
+            <input class="form-control" type="text" name='Descripcion' id='Descripcion' 
+            value="{{ isset($noticia->Descripcion)?$noticia->Descripcion:old('Descripcion') }}">
+            <br>
+        
+            <label for="Foto">Foto de la Noticia</label>
+            <input class="form-control" type="text" name='Foto' id='Foto' 
+            value="{{ isset($noticia->Foto)?$noticia->Foto:old('Foto') }}">
+            <br>
+        
+            <input class="btn btn-primary" type="submit" value='{{$modo}} Noticia'>
+        
+            <a class="btn btn-primary" href="{{ url('/noticia/') }}">Regresar</a>
+            <br>
+    </div>
 
-    @if(count($errors)>0)
-        <div class="alert alert-danger" role="alert">
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li> {{$error}} </li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <label for="Titulo">Titulo de la Noticia</label>
-    <input type="text" name='Titulo' id='Titulo' 
-    value="{{ isset($noticia->Titulo)?$noticia->Titulo:old('Titulo') }}">
-    <br>
-
-    <label for="Descripcion">Descripcion de la Noticia</label>
-    <input type="text" name='Descripcion' id='Descripcion' 
-    value="{{ isset($noticia->Descripcion)?$noticia->Descripcion:old('Descripcion') }}">
-    <br>
-
-    <label for="Foto">Foto de la Noticia</label>
-    <input type="text" name='Foto' id='Foto' 
-    value="{{ isset($noticia->Foto)?$noticia->Foto:old('Foto') }}">
-    <br>
-
-    <input type="submit" value='{{$modo}} Noticia'>
-
-    <a href="{{ url('/noticia/') }}">Regresar</a>
-    <br>
+</body>
+</html>
--- resources/views/noticia/index.blade.php
+++ resources/views/noticia/index.blade.php
@@ -1,25 +1,55 @@
-@if(Session::has('mensaje'))
-{{ Session::get('mensaje') }}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <title>Google News</title>
+</head>
 
-@endif
+<body>
+    <nav class="navbar navbar-light bg-white d-flex py-3 px-5 shadow p-3 mb-5">
+    
+        <a href="{{ url('/noticia/') }}">Google Noticias</a>
 
-<a href="{{ url('/noticia/') }}">Google Noticias</a>
-<a href="{{ url('/noticia/create') }}">Agregar Noticia</a>
-<div class="container">
-    @foreach ($noticias as $noticia)
-    <div>
-        <h2>{{ $noticia->Titulo }}</h2>
-        <h3>{{ $noticia->Descripcion }}</h3>
-        <img src="{{ $noticia->Foto }}" alt="">
+        
+        <div class="rounded input-group-prepend">
+            <input type="text" name="Buscar" placeholder="Buscar temas, ubicaciones y fuentes" id="Buscar" class="rounded bg-light form-control"/>
+        </div>
 
-        <a href="{{ url('/noticia/'.$noticia->id.'/edit') }}">Editar</a>
+        <a href="{{ url('/noticia/create') }}">Agregar Noticia</a>
+        
+    </nav>
+        
+    
+    @if(Session::has('mensaje'))
+    {{ Session::get('mensaje') }}
+    @endif
+    
+    <div class="container">
+        @foreach ($noticias as $noticia)
+        <div class="border rounded mb-3 p-3">
+            <h2>{{ $noticia->Titulo }}</h2>
+            <h3>{{ $noticia->Descripcion }}</h3>
 
-        <form action="{{ url('/noticia/'.$noticia->id) }}" method="post">
-            @csrf
-            {{ method_field('DELETE') }}
-            <input type="submit" onclick="return confirm('Seguro que deseas borrar la noticia?')" value="Borrar">
-        </form>
+            <div class="media">
+                <img src="{{ $noticia->Foto }}" alt="" class="float-right">
+            </div>
 
-    </div>
-    @endforeach
-</div>  
+            <a href="{{ url('/noticia/'.$noticia->id.'/edit') }}">Editar</a>
+    
+            <form action="{{ url('/noticia/'.$noticia->id) }}" method="post">
+                @csrf
+                {{ method_field('DELETE') }}
+                <input type="submit" onclick="return confirm('Seguro que deseas borrar la noticia?')" value="Borrar">
+            </form>
+    
+        </div>
+        @endforeach
+    </div>  
+    
+</body>
+</html>
+
